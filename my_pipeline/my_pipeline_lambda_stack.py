08bf5b2b9e69c195095811424964b91a83a3240d
Hibbert <stephibb@3c0630580352.ant.amazon.com>
2022-02-22 18:54:02 +0000

    Try AWS integration

--- my_pipeline/my_pipeline_lambda_stack.py
+++ my_pipeline/my_pipeline_lambda_stack.py
@@ -1,8 +1,9 @@
 from unicodedata import name
 import aws_cdk as cdk
 from constructs import Construct
-from aws_cdk.aws_lambda import Function, InlineCode, Runtime, IFunction
-from aws_cdk.aws_apigateway import RestApi, IRestApi, LambdaIntegration
+from aws_cdk.aws_lambda import Function, InlineCode, Runtime
+from aws_cdk.aws_apigateway import IRestApi, AwsIntegration
+from aws_cdk.aws_iam import ServicePrincipal
 
 class MyLambdaStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, referenced_api: IRestApi, **kwargs) -> None:
@@ -16,13 +17,23 @@ class MyLambdaStack(cdk.Stack):
             handler='index.main',
             runtime=Runtime.PYTHON_3_7,
         )
+        widget_function.function_arn
 
-        get_widgets_integration = LambdaIntegration(
-            widget_function,
-            allow_test_invoke=False,
-            request_templates={"application/json": '{ "statusCode": "200" }'}
+        # Not using Lambda integration because need manual control of permissions for cross-account
+        get_widgets_integration = AwsIntegration(
+            service="lambda",
+            path='2015-03-31/functions/${widget_function.function_arn}/invocations',
+            proxy=False
         )
 
-        referenced_api.root.add_method("GET", get_widgets_integration)   # GET /
+        get_widgets_method = referenced_api.root.add_method("GET", get_widgets_integration)   # GET /
+
+        api_principal = ServicePrincipal('apigateway.amazonaws.com')
+        
+        widget_function.add_permission("ApiInvokeLambdaPermissions",
+            principal=api_principal,
+            scope=get_widgets_method,
+            source_arn=get_widgets_method.method_arn
+        )
 
 
