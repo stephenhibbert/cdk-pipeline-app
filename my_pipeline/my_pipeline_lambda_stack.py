fc7cb4bd8f4b5d746015165a90ee38b5a645125f
Hibbert <stephibb@3c0630580352.ant.amazon.com>
2022-02-23 15:37:50 +0000

    powertools

--- my_pipeline/my_pipeline_lambda_stack.py
+++ my_pipeline/my_pipeline_lambda_stack.py
@@ -1,21 +1,21 @@
 import aws_cdk as cdk
 from constructs import Construct
-from aws_cdk.aws_lambda import Function, IFunction, Code, Runtime, Tracing
+from aws_cdk.aws_lambda import Function, IFunction, Code, Runtime, Tracing, LayerVersion
 from aws_cdk.aws_iam import ServicePrincipal
-from cdk_lambda_powertools_python_layer import LambdaPowertoolsLayer
 
 class MyLambdaStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        powertoolsLayer = LambdaPowertoolsLayer(self, 'PowertoolsLayer')
+        powertools_layer_arn = "arn:aws:lambda:eu-west-1:017000801446:layer:AWSLambdaPowertoolsPython:3"
+        powertools_layer = LayerVersion.from_layer_version_arn(self, "PowertoolsLayer", layer_version_arn=powertools_layer_arn)
 
         backend_function = Function(self, "myMainFunction",
             function_name=cdk.PhysicalName.GENERATE_IF_NEEDED,
             code=Code.from_asset("lambda"),
             handler='backend-lambda.handler',
             runtime=Runtime.PYTHON_3_9,
-            layers=[powertoolsLayer],
+            layers=[powertools_layer],
             tracing=Tracing.ACTIVE
         )
 
